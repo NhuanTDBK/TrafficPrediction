edc2be58391693e219d86aeb59e6e232af03a83a
NhuanTDBK <tranducnhuan1994@gmail.com>
2016-01-05 20:23:03 +0700

    Periodic

--- nnGridGeneticPeriodic.py
+++ nnGridGeneticPeriodic.py
@@ -74,8 +74,8 @@ list_ninput = np.arange(2,21)
 learning_rate = np.array([0.1,0.01,0.001,0.0001,0.00001,0.000001])
 list_results = np.zeros([ list_ninput.shape[0], learning_rate.shape[0] ],dtype=np.float64)
 for i in list_ninput:
-    print '.'
-    n_input = i
+    n_periodic=1
+    n_input = i+n_periodic
     from __init__ import *
     geneticEngine = PyEvolve(n_input)
     geneticEngine.fit()
